c0c6640a1ddba590d516964fc83085da6c82cf9d
Marambekova Ganjina <marambekovaganjina@gmail.com>
2025-08-20 16:37:57 +0500

    My homework

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+let fullname = "Raskritaya ladon";
+let author = "Sergey Plekhanov";
+let year = "2007";
+const isloggedin = true;
+let comment = "Ismaili and ismailism";
+
+
+
+
+
+console.log("Название" + fullname);
+console.log("Автор" + author);
+console.log("Год издания" + year);
+console.log("прочитала ли книгу" + (isloggedin? " yes ": "no"));
+console.log(" комментарии")
